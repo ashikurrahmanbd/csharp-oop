0b45ffc6b053d31b656ba9f089f00f6f2a04706a
Ashikur Rahman <proashik012@gmail.com>
2024-07-02 22:01:32 +0600

    Setter and getter

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography;
+
+
+class Person{
+    
+    public string name; //readable and writable
+
+    private int age; // not readable not writable
+
+    //we can make it readable and writable both 
+    // using setter function we can make it writable
+    // using getter function we can make it readble
+
+    public void Setter(int a){
+        age = a;
+    }
+
+    public int Getter(){
+        return age;
+    }
+
+
+}
+
+class MainClass{
+
+    // main Method
+    public static void Main(string[] args){
+     
+        Person p1 = new Person();
+
+        p1.name = "Ashik";
+        p1.Setter(29);
+
+        Console.WriteLine(p1.Getter());
+       
+    }
+
+}
+
+
+
+
